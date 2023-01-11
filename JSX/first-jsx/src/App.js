7bcd4f63563fd234e41bedaa40534f57bf03d9b4
CarsonNorman <95375043+CarsonNorman@users.noreply.github.com>
2023-01-11 13:47:47 -0800

    Update App.js

--- JSX/first-jsx/src/App.js
+++ JSX/first-jsx/src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 
 
 function App() {
+  let tasks = ['do something', 'do something else', 'do one last thing']
   return (
     <div className="App">
       <h1>Hello Dojo!</h1>
       <h3>Things I need to do</h3>
       <ul id='list' onClick >
-        <li>My homework</li>
-        <li>My laundry</li>
-        <li>Apply for jobs</li>
+        {tasks.map(task => <li>{task}</li>)}
       </ul>
     </div>
   );
