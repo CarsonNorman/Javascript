c4e640533eb7550b5ad60099461fef3739cab92e
CarsonNorman <95375043+CarsonNorman@users.noreply.github.com>
2023-01-18 13:38:59 -0800

    Update Info.jsx

--- api-walker/src/components/Info.jsx
+++ api-walker/src/components/Info.jsx
@@ -2,16 +2,23 @@ import { useState } from "react";
 import axios from 'axios'
 
 function Info() {
- 
+
     const [param, setParam] = useState('people')
     const [id, setId] = useState(null)
+    const [error, setError] = useState(null)
     const [data, setData] = useState(null)
 
     const getResults = (e) => {
         e.preventDefault()
         let url = 'https://swapi.dev/api/' + param + '/' + id
         axios.get(url)
-        .then(res => { setData(res); console.log(res.data) })
+            .then(res => { setData(res); setError(null) })
+            .catch(err => {
+                if (err) {
+                    setError('These are not the droid you are looking for');
+                    setData(null)
+                }
+            })
     }
     return (
         <div>
@@ -23,14 +30,20 @@ function Info() {
                 <input type="number" onChange={e => setId(e.target.value)} />
                 <button type="submit">Submit</button>
             </form>
+            {error &&
+                <div>
+                    <h5>{error}</h5>
+                    <img src='https://i.kym-cdn.com/entries/icons/mobile/000/018/682/obi-wan.jpg' alt="Ben Kenobi" />
+                </div>
+            }
             {data &&
-                
+
                 <div>
                     {(param === 'people') &&
                         <div>
                             <p>Name: {data.data.name} </p>
-                            <p>Height: {data.data.height} </p>
-                            <p>Mass: {data.data.mass} </p>
+                            <p>Height: {data.data.height} cm</p>
+                            <p>Mass: {data.data.mass} Kg</p>
                             <p>Skin Color: {data.data.skin_color} </p>
                         </div>
                     }
