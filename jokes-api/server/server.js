3c4cac1e33b917912a195821d3f347ae9c991742
CarsonNorman <95375043+CarsonNorman@users.noreply.github.com>
2023-01-20 11:48:58 -0800

    Node

--- jokes-api/server/server.js
+++ jokes-api/server/server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-require('./server/config/mongoose.config')
+require('./config/mongoose.config')
 
 app.use(express.json(), express.urlencoded({extended: false}))
-const routes = require('./server/routes/jokes.route')
+const routes = require('./routes/jokes.route')
 routes(app)
 
 app.listen(port, () => {
